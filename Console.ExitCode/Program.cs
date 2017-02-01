2cb493736f5d31fe58f5e818cbce089b8ef034ff
claudiu.glogovetean <claudiu.glogovetean@CLAUDIUPC>
2017-02-01 10:12:50 +0200

    Add project files.

---
+++ Console.ExitCode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Console.ExitCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Entered");
+
+            int value = int.Parse(args[0]);
+
+            switch (value)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    Environment.Exit(1);
+                    break;
+            }
+
+            System.Console.WriteLine("Leave");
+        }
+    }
+}
---
+++ Console.ExitCode/Run.ps1
@@ -0,0 +1,8 @@
+﻿$testProcess = Start-Process -FilePath 'Console.ExitCode.exe' -ArgumentList '1'  -PassThru 
+Wait-Process -Id $testProcess.Id -Timeout 120
+if ( $testProcess.ExitCode -eq 0){
+	Write-Output "Success"
+} 
+if ($testProcess.ExitCode -eq 1){
+Write-Error "Failure"
+}
